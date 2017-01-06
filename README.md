d22701e15049dd494969449bba0ffe618ab1978c
Paulo Chaves <opaulochaves@gmail.com>
2017-01-06 13:30:30 -0300

    setup environment to run react

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-begin
+
+### A big thanks to
+[Setting up Webpack, Babel and React from scratch](https://stanko.github.io/setting-up-webpack-babel-and-react-from-scratch/)
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <div className='Menu'>
+        <h1>Hi Paulo!</h1>
+        <h2>You are awesome!</h2>
+      </div>
+    );
+  }
+}
